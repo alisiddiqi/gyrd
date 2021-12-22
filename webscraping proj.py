5f0e166bfdbcc06f9f25e66a68611859169c9144
alvzhou <alvinzen9@gmail.com>
2021-12-22 14:19:51 -0800

    added chromedriver

--- webscraping proj.py
+++ webscraping proj.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 driver = webdriver.Chrome(executable_path= './drivers/chromedriver')
-driver.get('https://lots.impark.com/imp/en?latlng=49.895136,-97.138374&q=Winnipeg%20undefined%20undefined')
+driver.get('https://lots.impark.com/imp/en?latlng=53.546125,-113.493823&q=Edmonton%20undefined%20undefined')
 
 names = []
 address = [] #address of parking spots
@@ -16,11 +16,12 @@ lot_names = soup.findAll('div', attrs={'class':'lot-name'})
 addresses = soup.findAll('span', attrs={'class':'address-text'})
 prices = soup.findAll('div', attrs={'class':'lot-rate'})
 
+
 for name in lot_names:
     names.append(name.text)
 
 for a in addresses:
-    address.append(a.text + "Winnipeg, MB")
+    address.append(a.text + "Edmonton, AB")
 
 for p in prices:
     price_per_hour.append(p.text)
@@ -29,7 +30,7 @@ print(len(names))
 print(len(address))
 print(len(price_per_hour))
 df = pd.DataFrame({'Lot Name': names, 'Address': address, 'Price': price_per_hour})
-df.to_csv('winnipegparking2.csv', index=False, encoding='utf-8')
+df.to_csv('EdmontonImPark.csv', index=False, encoding='utf-8')
 
 
 #hello
